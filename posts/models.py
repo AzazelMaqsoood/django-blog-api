ed0fa660d1fdf023bbe20ff8b10bfafc6d017814
John Doe <max-essential@bk.ru>
2026-03-12 17:42:07 +0300

    register models in admin and add created_at

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=100)
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Комментарий к посту{self.post.title}"
+        return f"Комментарий к посту {self.post.title}"
     
