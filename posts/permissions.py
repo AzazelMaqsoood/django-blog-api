685e7d1a6cfae5cb8a22c42405a21e3ce63cd400
John Doe <max-essential@bk.ru>
2026-03-13 16:08:43 +0300

    correct typo in IsAuthorOrReadOnly permission

--- posts/permissions.py
+++ posts/permissions.py
@@ -2,6 +2,6 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.methon in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
