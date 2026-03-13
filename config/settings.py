6e1a63f25647b167f0a6b7411f66c553a40eaea9
John Doe <max-essential@bk.ru>
2026-03-13 18:04:54 +0300

    configure Celery eager mode for tests

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,7 @@ INTERNAL_IPS = [
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
+
+# Настройки для тестов (Celery работает синхронно)
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
