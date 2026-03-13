5120050128c5bb92d12630a0a9fe341aa6be40eb
John Doe <max-essential@bk.ru>
2026-03-13 12:51:11 +0300

    update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
-router.register(r'comment', CommentViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
