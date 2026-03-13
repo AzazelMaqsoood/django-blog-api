d626e7a02916dfa03aa2f68549f2f35f4f3bdcfa
John Doe <max-essential@bk.ru>
2026-03-13 13:09:59 +0300

    nested comments serialization with source attribute

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from .models import Post, Comment
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True, source='comment_set')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, PostDetailSerializer
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return PostDetailSerializer
+        return PostSerializer
+
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
