38d852386d8ef05fa3014ef0cfa50c33270049ee
John Doe <max-essential@bk.ru>
2026-03-13 12:11:11 +0300

    setup URL routing for API

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comment', CommentViewSet)
+
+urlpatterns = router.urls
